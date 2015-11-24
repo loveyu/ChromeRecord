9f08942ba4b4bbda0d41896a751f001aad18976b
loveyu <admin@loveyu.info>
2015-11-24 10:32:16 +0800

    优化背景数据的提交

--- chrome_record.php
+++ chrome_record.php
@@ -6,16 +6,33 @@
  */
 error_reporting(E_ALL);
 $request = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : file_get_contents("php://input");
-if (empty($request)) {
-    return;
+if(empty($request)) {
+	return;
 }
 $object = json_decode($request, true);
-if (!is_array($object) || empty($object) || !isset($object['url'])) {
-    return;
+if(!is_array($object) || empty($object) || !isset($object[0])) {
+	return;
 }
-$m = new MongoClient();
-$db = $m->chrome_record;
-$collection = $db->$object['type'];
-$object['time'] = time();
-$object['ua'] = $_SERVER['HTTP_USER_AGENT'];
-$collection->insert($object);
+$time = time();
+$is_mongo = false;
+$db = null;
+if($is_mongo) {
+	$m = new MongoClient();
+	$db = $m->chrome_record;
+	$collection_map = [];
+} else {
+	file_put_contents("E:\\cr.log", print_r($object, true));
+}
+foreach($object as $v) {
+	$object['time'] = time();
+	$object['ua'] = $_SERVER['HTTP_USER_AGENT'];
+
+	if($is_mongo) {
+		if(!isset($collection_map[$v['type']])) {
+			$collection_map[$v['type']] = $db->$object['type'];
+		}
+		$collection_map[$v['type']]->insert($v);
+	}
+}
+echo "OK";
+exit;
--- source/background.js
+++ source/background.js
@@ -1,16 +1,27 @@
 var url_rule = {urls: ["<all_urls>"]};
 var server_url = "http://127.0.0.1/chrome_record.php";
+var queue = [];
+var counter = 0;
 
 var log_func = function (url, detail, type, requestId) {
 	if (url == server_url) {
 		return;
 	}
 	var obj = {url: url, type: type, requestId: requestId, detail: detail};
-	setTimeout(function () {
-		var req = new XMLHttpRequest();
-		req.open("POST", server_url, true);
-		req.send(JSON.stringify(obj));
-	}, 0);
+	queue.push(obj);
+	if (counter < 1) {
+		setTimeout(function () {
+			counter--;
+			if (queue.length > 0) {
+				var tmp = queue;
+				queue = [];
+				var req = new XMLHttpRequest();
+				req.open("POST", server_url, true);
+				req.send(JSON.stringify(tmp));
+			}
+		}, 1000);
+		counter++;
+	}
 };
 
 chrome.webRequest.onBeforeRequest.addListener(function (info) {
